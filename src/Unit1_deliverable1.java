e5c5aef67076a77b03b99ee79bfc254cbfb24209
David Muhsin <dmuhsin@gmail.com>
2022-03-10 22:34:27 -0500

    Completed Deliverable

--- src/Unit1_deliverable1.java
+++ src/Unit1_deliverable1.java
@@ -1,8 +1,44 @@
-
+import java.util.Scanner;
 public class Unit1_deliverable1 {
 
 	public static void main(String[] args) {
-		System.out.print("Test");
+		
+		
+		boolean userPassHasUpper = false;
+		boolean userPassHasLower = false;
+		boolean userPassHasExclamation = false;
+		boolean endLoop = false;
+		
+		while(endLoop == false) {
+			System.out.print("\n Please Create A Password: ");
+			Scanner input = new Scanner(System.in);
+			String userPass = input.nextLine();
+			int userPassLen = userPass.length();
+			
+			for(char ch: userPass.toCharArray()) {
+				if(Character.isUpperCase(ch)==true) {
+					userPassHasUpper = true;				
+				}
+				else if (Character.isLowerCase(ch)==true) {
+					userPassHasLower = true;
+				}
+				else if (ch=='!') {
+					userPassHasExclamation = true;
+				}
+			}
+			if(userPassHasUpper==true & userPassHasLower==true & userPassHasExclamation==true & userPassLen>=7 & userPassLen<=12 ) {
+				System.out.print("Password valid and accepted");
+				endLoop = true;
+			}
+			else {
+				System.out.print("Error");
+			}
+			
+			
+			
+			
+			
+		}
 
 	}
 
